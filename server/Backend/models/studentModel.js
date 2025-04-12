8393943f9f16abd3edf6a08e481d304fe3720c6f
Devkumar Patel <dpat1072@my.centennialcollege.ca>
2025-04-12 14:50:02 -0400

    Corrected typo in export default Student

--- server/Backend/models/studentModel.js
+++ server/Backend/models/studentModel.js
@@ -1,17 +1,17 @@
 import mongoose from 'mongoose';
 
 const studentSchema = new mongoose.Schema({
-    studentNumber: {type: String, required: true, unique: true},
-    password: {type: String, required: true},
-    firstName: {type: String, required: true},
-    lastName: {type: String, required: true},
-    email: {type: String, required: true, unique: true},
-    phoneNumber: {type: String, required: true},
-    address: {type: String, required: true},
-    city: {type: String, required: true},
-    program: {type: String, required: true},
-    favouriteCourse: {type: String, required: true},
-    hobby: {type: String, required: true},
+    studentNumber: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    phoneNumber: { type: String, required: true },
+    address: { type: String, required: true },
+    city: { type: String, required: true },
+    program: { type: String, required: true },
+    favouriteCourse: { type: String, required: true },
+    hobby: { type: String, required: true }
 });
 
 const Student = mongoose.model('Student', studentSchema);
